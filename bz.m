d5b40302d295757b00a5cbd701a6f5d3959089cf
mejironochaos <boukendeshodeshokuroiwa@yahoo.co.jp>
2016-11-23 19:22:58 +0900

    Add files via upload

---
+++ bz.m
@@ -0,0 +1,8 @@
+ai=[1 0 0]
+aj=[0 1 0]
+ak=[0 0 1]
+bi=2\pi/dot(ai,cross(aj,ak))*cross(aj,ak)
+bj=2\pi/dot(aj,cross(ak,ai))*cross(ak,ai)
+bk=2\pi/dot(ak,cross(ai,aj))*cross(ai,aj) 
+!gyakukousi vector
+
